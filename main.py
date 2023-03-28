1eba0e0abd319658eb705fadc3fe3bb05a32e1ba
MPacris <mpacris@yahoo.com>
2023-03-28 15:01:16 -0500

    completed task 2 of lab 2, placed into main and pushing up

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 #Write code that takes a string as input and returns the string reversed
 #i.e. “Hello” will be returned as “olleH
 
+word = input('provide a word')
+
 def reverse_word(word):
     reversed_word = ""     
     last_index = len(word) - 1
@@ -11,6 +13,23 @@ def reverse_word(word):
         
     print(reversed_word)
 
-word = input('provide a word')
+
     
 reversed_word = reverse_word(word)
+
+
+#Task 2: Capitalize a Letter
+#Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World
+
+#Thoughts:  split "hello world" into two words and capitalize first letter --- found a function doing research that created something similar to a title
+
+the_given_string = input('proved a multi-word string    ')
+
+def capitalize_first_letter(the_given_string):
+    capitalized_words = the_given_string.title()
+    print(capitalized_words)     
+      
+
+capitalized_words = capitalize_first_letter(the_given_string)
+
+
--- sandbox.py
+++ sandbox.py
@@ -3,25 +3,20 @@
 #Write code that takes a string as input and returns the string reversed
 #i.e. “Hello” will be returned as “olleH
 
-def reverse_word(word):
-    reversed_word = ""     
-    last_index = len(word) - 1
-    for index in range(last_index, -1, -1):
-        reversed_word += word[index]
-        
-    print(reversed_word)
-
-word = input('provide a word')
-    
-reversed_word = reverse_word(word)
-
-
-
-
-
 
 #Task 2: Capitalize a Letter
 #Write code that takes a string as input and capitalize the first letter of each word. Words will be separated by only one space. i.e. “hello world” should be outputted as “Hello World
+
+#Thoughts:  split "hello world" into two words and capitalize first letter --- found a function doing research that created something similar to a title
+
+the_given_string = input('proved a multi-word string    ')
+
+def capitalize_first_letter(the_given_string):
+    capitalized_words = the_given_string.title()
+    print(capitalized_words)     
+      
+
+capitalized_words = capitalize_first_letter(the_given_string)
 
 
 
